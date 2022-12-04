e1087c605c45149f905c00ced251048d55fc76d3
javidkolurazhan <javidklurajan@gmail.com>
2022-12-04 23:43:28 +0330

    register service

--- RabitMqProducer/Program.cs
+++ RabitMqProducer/Program.cs
@@ -1,3 +1,6 @@
+using RabitMqProducer.Data;
+using RabitMqProducer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ShopContext>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
